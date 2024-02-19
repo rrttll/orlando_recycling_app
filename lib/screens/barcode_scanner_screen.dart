39c7b472411a4c857549ae404742d0867742cfb3
Richard <rtlloyd@gmail.com>
2024-02-19 18:18:14 -0500

    barcode scanner not working

--- lib/screens/barcode_scanner_screen.dart
+++ lib/screens/barcode_scanner_screen.dart
@@ -1,12 +1,56 @@
 //File: lib/screens/barcode_scanner_screen.dart
-import 'package:flutter/material.dart';
 
-class BarcodeScannerScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class BarcodeScannerScreen extends StatefulWidget {
+  @override
+  _BarcodeScannerScreenState createState() => _BarcodeScannerScreenState();
+}
+
+class _BarcodeScannerScreenState extends State<BarcodeScannerScreen> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+  String qrText = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Barcode Scanner")),
-      body: Center(child: Text("This is the Barcode Scanner Screen")),
+      appBar: AppBar(
+        title: Text('Barcode Scanner'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Text('Scan result: $qrText'),
+            ),
+          )
+        ],
+      ),
     );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        qrText = scanData.code;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
