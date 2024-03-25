f26c98bb5e99c812cda986a158cbe2636daf3505
StarNight346 <adambates364@gmail.com>
2024-03-24 17:19:53 -0700

    Worked on the Barcode scanner

--- lib/screens/barcode_scanner_screen.dart
+++ lib/screens/barcode_scanner_screen.dart
@@ -1,12 +1,55 @@
 //File: lib/screens/barcode_scanner_screen.dart
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+//import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
-class BarcodeScannerScreen extends StatelessWidget {
+
+class BarcodeScannerScreen extends StatefulWidget {
+  @override
+  BarcodeScannerScreenState createState() => BarcodeScannerScreenState();
+}
+
+class BarcodeScannerScreenState extends State<BarcodeScannerScreen> {
+  String? scanResult;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Barcode Scanner")),
-      body: Center(child: Text("This is the Barcode Scanner Screen")),
+      body: Center(
+        child: Column( 
+          children: <Widget> [ ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.lightBlue,
+            ),
+            onPressed: scanBarcode, icon: Icon(Icons.abc), label: Text("Scan a barcode")),
+            Text("This is the Barcode Scanner Screen here is the result: $scanResult")],
+       )
+     )
     );
   }
+
+
+
+Future scanBarcode() async {
+  String scanResult;
+  try {
+    scanResult = await FlutterBarcodeScanner.scanBarcode(
+      "ff6666",
+      "Cancel",
+      true,
+      ScanMode.BARCODE,
+    );
+  } catch (ex) {
+    print(ex);
+    scanResult = "Failed to retrieve results";
+  } 
+  // on Exception {
+  //   scanResult = "Failed to retrieve results";
+  // } 
+
+setState(() => this.scanResult = scanResult);
+
+}
 }
